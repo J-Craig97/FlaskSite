8e23630d57a0b4f35e2d368c07920d861dc7345e
Josh Craig <josh.craig7897@gmail.com>
2020-11-07 17:32:02 +0000

    First commit

---
+++ flaskProject/pypi_org/app.py
@@ -0,0 +1,30 @@
+import flask
+from flask import Flask
+
+from pypi_org.infrastructure.view_modifiers import response
+
+app = Flask(__name__)
+
+def get_latest_packages():
+    return [
+        {'name': 'flask', 'version': '1.2.3'},
+        {'name': 'python', 'version': '3.9'},
+        {'name': 'sqlAlchemy', 'version': '2.2.0'}
+    ]
+
+@app.route('/')
+@response(template_file='home/index.html')
+def index():
+    test_packages = get_latest_packages()
+    return {'packages': test_packages}
+    #return flask.render_template('home/index.html', packages=test_packages)
+
+@app.route('/about')
+@response(template_file='home/about.html')
+def about():
+    return {}
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
